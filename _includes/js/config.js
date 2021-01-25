f3138d6099cefc5d6b0a1f804aad6d035139a2d0
ErlendNilsen <erlend.nilsen@nina.no>
2021-01-25 22:45:00 +0100

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -3,5 +3,5 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', ext
 }});
 
 var siteConfig = {
-  rootPredicate: { type: 'equals', key: 'country', value: "NO" }
+  rootPredicate: { type: 'equals', key: 'publishingCountry', value: "NO" }
 };
