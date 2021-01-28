d2611fdcd5398423ab62536b8b0150be73e33244
ErlendNilsen <erlend.nilsen@nina.no>
2021-01-28 23:17:32 +0100

    Changed scope to NINA

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -3,5 +3,5 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', ext
 }});
 
 var siteConfig = {
-  rootPredicate: { type: 'equals', key: 'countryCode', value: "NO" }
+  rootPredicate: { type: 'equals', key: 'publishingInstitution', value: "Norwegian Institute for Nature Research" }
 };
