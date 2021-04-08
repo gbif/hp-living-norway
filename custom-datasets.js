d77506049ed9487aeedd59c9d6a25851b45228e1
NINA\siw.berge <siw.berge@nina.no>
2021-04-08 11:31:52 +0200

    #4: First draft dataset page

---
+++ custom-datasets.js
@@ -0,0 +1,31 @@
+
+
+ const datasetListElement = document.getElementById('my-datasets');
+ const datasetTemplate = datasets => `
+  <ul>
+  ${datasets.results.map(x => `<li><a href="dataset?key=${x.key}">${x.title}</a></li>`).join('')}
+  </ul>
+ `;
+
+ var datasetTemp = function(datasets) {
+  `
+  <div>${JSON.stringify(datasets)}</div>
+  <ul>
+  
+  ${datasets.results.map(x => `<li><a href="dataset?key=${x.key}">${x.title}</a></li>`)}
+  </ul>
+ `
+ };
+
+
+fetch(`https://api.gbif.org/v1/network/379a0de5-f377-4661-9a30-33dd844e7b9a/constituents`)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonResponse) {
+    datasetListElement.innerHTML = datasetTemplate(jsonResponse);
+    
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
---
+++ datasets.md
@@ -0,0 +1,12 @@
+---
+layout: compose
+klass: dataset
+permalink: /datasets
+composition:
+- type: pageMarkdown
+---
+
+
+<h1>Available datasets</h1>
+<article id="my-datasets"></article>
+<script src="custom-datasets.js">
