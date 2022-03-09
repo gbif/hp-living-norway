da7a4145fb36dfebcb27c06d9d471214797acce9
Morten Høfft <mhoefft@gbif.org>
2022-03-09 14:23:07 +0100

    feat: update configuration schema. See https://github.com/gbif/hosted-portals/issues/221

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -3,11 +3,16 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', ext
 }});
 
 var siteConfig = {
-  routeConfig: {
+  routes: {
+    occurrenceSearch: {
+      // The route you are currently using for occurrence search. The language prefix will be added automatically
+      // If you need special routes per language, then you have to add locale specific overwrites. The page language is available as a global variable called `pageLang`
+      route: '/data'
+    },
     datasetKey: {
       url: ({key}) => `/dataset?key=${key}`,
       isHref: true
-    },
+    }
   },
   occurrence: {
     rootPredicate: { type: 'equals', key: 'networkKey', value: "379a0de5-f377-4661-9a30-33dd844e7b9a" }
