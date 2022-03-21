4f4c8daea250d80fde71ed167e52bd2c8c492d0f
Francesco Frassinelli <francesco.frassinelli@nina.no>
2022-03-21 09:13:37 +0100

    Add code for Clouflare Workers script (#24)

---
+++ scripts/cloudflare-workers.js
@@ -0,0 +1,59 @@
+addEventListener("fetch", (event) => {
+  event.respondWith(
+    handleRequest(event.request).catch(
+      (err) => new Response(err.stack, { status: 500 })
+    )
+  );
+});
+
+async function gbifQuery(query, variables) {
+  return fetch('https://graphql.gbif-staging.org/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify({
+      query: query,
+      variables: variables,
+    })
+  })
+}
+
+async function handleRequest(request) {
+  const { pathname } = new URL(request.url);
+
+  if (pathname == "/literature") {
+    // https://github.com/gbif/content-crawler/issues/46
+    const MAX_LIMIT = 2**31-1;
+
+    datasets = await (await gbifQuery(`{ 
+      datasetSearch(networkKey: "379a0de5-f377-4661-9a30-33dd844e7b9a", limit: ${MAX_LIMIT}) {
+        results {
+            key
+        }
+      }
+    }`)).json();
+    documents = await (await gbifQuery(`
+      query($keys: [ID]){
+        literatureSearch(gbifDatasetKey: $keys) {
+          documents { count }
+        }
+      }`,
+      {
+        keys: datasets.data.datasetSearch.results.map(d => d.key),
+      },
+    )).json();
+    return new Response(JSON.stringify({
+        count: documents.data.literatureSearch.documents.count,
+      }), {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET",
+       }
+    });
+  }
+ 
+  return fetch("https://livingnorway.no/");
+}
