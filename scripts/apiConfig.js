f826945ef5a352b40f51d2d63c55f07083b9113f
NINA\siw.berge <siw.berge@nina.no>
2022-04-06 12:43:06 +0200

    Put url's in config file, add vue production mode + small textual improvements

---
+++ scripts/apiConfig.js
@@ -0,0 +1,5 @@
+window.apiConfig = {
+    "gbif-graphql" :  "https://graphql.gbif.org/graphql",
+    "gbif-api-v1" :  "https://api.gbif.org/v1/",
+    "gbif-api-v2" :  "https://api.gbif.org/v2/"  
+}
